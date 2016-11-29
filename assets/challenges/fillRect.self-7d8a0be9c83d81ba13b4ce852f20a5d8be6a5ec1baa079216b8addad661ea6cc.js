79873f054b4892ff1806d0d2d696820df76e6370
Jared Cosulich <jared.cosulich@gmail.com>
2016-11-29 11:57:59 -0500

    challenge 3 for fill_rect

--- assets/challenges/fillRect.self-7d8a0be9c83d81ba13b4ce852f20a5d8be6a5ec1baa079216b8addad661ea6cc.js
+++ assets/challenges/fillRect.self-7d8a0be9c83d81ba13b4ce852f20a5d8be6a5ec1baa079216b8addad661ea6cc.js
@@ -1,11 +1,12 @@
 (function() {
-  var initFillRectChallenge1, initFillRectChallenge2, initFillRectChallenges;
+  var initFillRectChallenge1, initFillRectChallenge2, initFillRectChallenge3, initFillRectChallenges;
 
   initFillRectChallenges = function() {
     var page;
     if ((page = $('#fill_rect')).length > 0) {
       initFillRectChallenge1(page);
-      return initFillRectChallenge2(page);
+      initFillRectChallenge2(page);
+      return initFillRectChallenge3(page);
     }
   };
 
@@ -63,6 +64,46 @@
           }
         }
         message = success ? '<strong>Success!</strong> Your rectangle is covering the x\'s!' : 'Nice try, but you need to draw a rectangle that covers both x\'s.';
+        return canvas.alert(message, success);
+      }), 200);
+    });
+  };
+
+  initFillRectChallenge3 = function(page) {
+    var canvas, challenge, editor, points;
+    challenge = page.find('#challenge3');
+    canvas = new App.Canvas(challenge.find('canvas'));
+    editor = new App.Editor(challenge.find('.editor'), canvas);
+    points = [
+      new Test.Point({
+        x: 100,
+        y: 150,
+        colors: [15, 39, 143],
+        canvas: canvas
+      }), new Test.Point({
+        x: 200,
+        y: 150,
+        colors: [255, 255, 255],
+        canvas: canvas
+      }), new Test.Point({
+        x: 300,
+        y: 150,
+        colors: [219, 84, 67],
+        canvas: canvas
+      })
+    ];
+    return challenge.find('.run').click(function() {
+      return setTimeout((function() {
+        var i, len, message, point, success;
+        success = true;
+        for (i = 0, len = points.length; i < len; i++) {
+          point = points[i];
+          if (point.test() < 1) {
+            success = false;
+            break;
+          }
+        }
+        message = success ? '<strong>Success!</strong> Your rectangle is covering the x\'s with the right colors!' : 'Nice try, but you need to draw rectangles that cover each x in the right color.';
         return canvas.alert(message, success);
       }), 200);
     });
