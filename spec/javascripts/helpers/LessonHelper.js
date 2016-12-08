7eee2052d498426041ed9baa09186bbe938d58c9
Jared Cosulich <jared.cosulich@gmail.com>
2016-12-07 20:26:17 -0500

    refactoring test infrastructure

--- spec/javascripts/helpers/LessonHelper.js
+++ spec/javascripts/helpers/LessonHelper.js
@@ -1,72 +1,77 @@
-function createPage(pageId, challengeCount) {
-  var page = $(document.createElement('DIV'));
-  page.attr('id', pageId);
-
-  for (var i=0; i<challengeCount; ++i) {
-    var challengeId = 'challenge' + (i + 1);
-    var challenge = $(document.createElement('DIV'));
-    challenge.attr('id', challengeId);
-
-    var codeEditor = $(document.createElement('DIV'));
-    codeEditor.addClass('code-editor');
-
-    var editor = $(document.createElement('DIV'));
-    editor.addClass('editor')
-    codeEditor.append(editor);
-
-    var buttons = $(document.createElement('DIV'));
-    buttons.addClass('buttons');
-    var run = $(document.createElement('DIV'));
-    run.addClass('run');
-    buttons.append(run);
-    codeEditor.append(buttons);
-    challenge.append(codeEditor);
-
-    var visual = $(document.createElement('DIV'));
-    visual.addClass('visual');
-
-    var canvas = $(document.createElement('CANVAS'));
-    canvas.attr('id', pageId + '_' + challengeId);
-    visual.append(canvas);
-
-    var alert = $(document.createElement('DIV'));
-    alert.addClass('alert');
-    visual.append(alert);
-    challenge.append(visual);
-
-    challenge.css({position: 'absolute', top: '-1000px', left: '-1000px'})
-
-    page.append(challenge);
+class TestPage {
+  constructor(pageId, challengeCount) {
+    this.pageId = pageId;
+    this.challengeCount = challengeCount;
+    this.buildPage();
+    $(document).trigger('initialization:complete')
   }
-  $(document.body).append(page);
-  return page;
-}
 
-// .code-editor.col-xs-6
-//   %h5
-//     Editor
-//     %small (write code below)
-//   .editor{id: editor_id}<
-//     = yield
-//
-//   .log.bg-faded.text-muted.small
-//     %button.close{"aria-label" => "Close", :type => "button"}
-//       %span{"aria-hidden" => "true"} ×
-//     .title
-//       %strong Message Log
-//     .messages
-//
-//   .buttons.py-1
-//     .run.btn.btn-success
-//       Run
-//     .reset.btn.btn-danger.float-xs-right
-//       Reset
-//
-// .visual.col-xs-6
-//   %h5
-//     Canvas
-//     %small (your drawing will display here)
-//   .canvas
-//     .background.hidden-xl-down= image_tag('lessons/fill_rect/challenge1.png')
-//     %canvas#fill_rect_challenge1
-//   .alert
+  buildPage() {
+    this.page = $(document.createElement('DIV'));
+    this.page.attr('id', this.pageId);
+
+    this.challengeEditors = [];
+    this.challengeRunButtons = [];
+    this.challengeCanvases = [];
+    this.challengeAlerts = [];
+    for (var i=0; i<this.challengeCount; ++i) {
+      var challengeId = 'challenge' + (i + 1);
+      var challenge = $(document.createElement('DIV'));
+      challenge.attr('id', challengeId);
+
+      var codeEditor = $(document.createElement('DIV'));
+      codeEditor.addClass('code-editor');
+
+      var editor = $(document.createElement('DIV'));
+      editor.addClass('editor')
+      codeEditor.append(editor);
+
+      this.challengeEditors.push(editor);
+
+      var buttons = $(document.createElement('DIV'));
+      buttons.addClass('buttons');
+      var run = $(document.createElement('DIV'));
+      run.addClass('run');
+      buttons.append(run);
+      codeEditor.append(buttons);
+      challenge.append(codeEditor);
+
+      this.challengeRunButtons.push(run);
+
+      var visual = $(document.createElement('DIV'));
+      visual.addClass('visual');
+
+      var canvas = $(document.createElement('CANVAS'));
+      canvas.attr('id', this.pageId + '_' + challengeId);
+      visual.append(canvas);
+
+      this.challengeCanvases.push(canvas);
+
+      var alert = $(document.createElement('DIV'));
+      alert.addClass('alert');
+      visual.append(alert);
+      challenge.append(visual);
+
+      this.challengeAlerts.push(alert)
+
+      challenge.css({position: 'absolute', top: '-1000px', left: '-1000px'})
+
+      this.page.append(challenge);
+    }
+    $(document.body).append(this.page);
+  }
+
+  runChallengeCode(challengeNumber, code) {
+    var aceEditor = this.challengeEditors[challengeNumber - 1].data('ace');
+    aceEditor.setValue(code);
+    this.challengeRunButtons[challengeNumber - 1].click()
+  }
+
+  challengeResult(challengeNumber) {
+    return this.challengeAlerts[challengeNumber - 1].hasClass('alert-success')
+  }
+
+  destroy() {
+    this.page.remove();
+  }
+}
--- spec/javascripts/lessons/FillRectSpec.js
+++ spec/javascripts/lessons/FillRectSpec.js
@@ -1,25 +1,17 @@
 describe("FillRect", function() {
   var page;
   beforeEach(function() {
-    page = createPage('fill_rect', 3);
-    $(document).trigger('initialization:complete')
+    page = new TestPage('fill_rect', 3);
   });
 
   describe("Challenge 1", function() {
-    var challenge, editor, run, alert;
-
     beforeEach(function() {
       jasmine.clock().install();
-      challenge = page.find('#challenge1');
-      editor = challenge.find('.editor').data('ace');
-      canvas = challenge.find('canvas');
-      run = challenge.find('.run');
-      alert = challenge.find('.alert');
     });
 
     afterEach(function() {
       jasmine.clock().uninstall();
-      page.remove();
+      page.destroy();
     });
 
     it("succeeds with good solution", function() {
@@ -31,10 +23,9 @@ describe("FillRect", function() {
               context.fillRect(50,50,100,100); \
               context.stroke();"
 
-      editor.setValue(code);
-      run.click();
+      page.runChallengeCode(1, code)
       jasmine.clock().tick(200);
-      expect(alert.hasClass('alert-success')).toBe(true);
+      expect(page.challengeResult(1)).toBe(true);
     });
 
     it("fails with bad solution", function() {
@@ -46,10 +37,9 @@ describe("FillRect", function() {
               context.fillRect(150,150,100,100); \
               context.stroke();"
 
-      editor.setValue(code);
-      run.click();
+      page.runChallengeCode(1, code)
       jasmine.clock().tick(200);
-      expect(alert.hasClass('alert-danger')).toBe(true);
+      expect(page.challengeResult(1)).toBe(false);
     });
   });
 
