24865ce21e2614ddcab79b56561b14d6cc28b268
Jared Cosulich <jared.cosulich@gmail.com>
2017-01-14 21:07:54 -0500

    new version

--- assets/lessons/flappy_square/stage2.self-39df139bb1e613635569ad4755aeaf69d7a922c592bb87832f25ad155203ee51.js
+++ assets/lessons/flappy_square/stage2.self-39df139bb1e613635569ad4755aeaf69d7a922c592bb87832f25ad155203ee51.js
@@ -1,5 +1,5 @@
 (function() {
-  var initFlappySquareStage2Challenge1, initFlappySquareStage2Challenge2, initFlappySquareStage2Challenge3, initFlappySquareStage2Challenge4, initFlappySquareStage2Challenge5, initFlappySquareStage2Challenges;
+  var initFlappySquareStage2Challenge1, initFlappySquareStage2Challenge2, initFlappySquareStage2Challenge3, initFlappySquareStage2Challenge4, initFlappySquareStage2Challenge5, initFlappySquareStage2Challenge6, initFlappySquareStage2Challenges;
 
   initFlappySquareStage2Challenges = function() {
     var page;
@@ -8,69 +8,90 @@
       initFlappySquareStage2Challenge2(page);
       initFlappySquareStage2Challenge3(page);
       initFlappySquareStage2Challenge4(page);
-      return initFlappySquareStage2Challenge5(page);
+      initFlappySquareStage2Challenge5(page);
+      return initFlappySquareStage2Challenge6(page);
     }
   };
 
   initFlappySquareStage2Challenge1 = function(page) {
-    var canvas, challenge, editor;
-    challenge = page.find('#challenge1');
+    var canvas, challenge, challengeIndex, editor;
+    challengeIndex = 1;
+    challenge = page.find("#challenge" + challengeIndex);
     canvas = new App.Canvas(challenge.find('canvas'));
     editor = new App.Editor(challenge.find('.editor'), canvas);
     return challenge.find('.run').click(function() {
       return setTimeout((function() {
         return canvas.selfAssess(function() {
-          return App.currentProgress.challengeComplete('flappy_square_stage2', 'challenge1');
+          return App.currentProgress.challengeComplete('flappy_square_stage2', "challenge" + challengeIndex);
         });
-      }), 1000);
+      }), 2000);
     });
   };
 
   initFlappySquareStage2Challenge2 = function(page) {
-    var canvas, challenge, editor;
-    challenge = page.find('#challenge2');
+    var canvas, challenge, challengeIndex, editor;
+    challengeIndex = 2;
+    challenge = page.find("#challenge" + challengeIndex);
     canvas = new App.Canvas(challenge.find('canvas'));
     editor = new App.Editor(challenge.find('.editor'), canvas);
     return challenge.find('.run').click(function() {
       return setTimeout((function() {
         return canvas.selfAssess(function() {
-          return App.currentProgress.challengeComplete('flappy_square_stage2', 'challenge2');
+          return App.currentProgress.challengeComplete('flappy_square_stage2', "challenge" + challengeIndex);
         });
       }), 2000);
     });
   };
 
   initFlappySquareStage2Challenge3 = function(page) {
-    var canvas, challenge, editor;
-    challenge = page.find('#challenge3');
+    var canvas, challenge, challengeIndex, editor;
+    challengeIndex = 3;
+    challenge = page.find("#challenge" + challengeIndex);
     canvas = new App.Canvas(challenge.find('canvas'));
     editor = new App.Editor(challenge.find('.editor'), canvas);
     return challenge.find('.run').click(function() {
       return setTimeout((function() {
         return canvas.selfAssess(function() {
-          return App.currentProgress.challengeComplete('flappy_square_stage2', 'challenge3');
+          return App.currentProgress.challengeComplete('flappy_square_stage2', "challenge" + challengeIndex);
         });
       }), 2000);
     });
   };
 
   initFlappySquareStage2Challenge4 = function(page) {
-    var canvas, challenge, editor;
-    challenge = page.find('#challenge4');
+    var canvas, challenge, challengeIndex, editor;
+    challengeIndex = 4;
+    challenge = page.find("#challenge" + challengeIndex);
     canvas = new App.Canvas(challenge.find('canvas'));
     editor = new App.Editor(challenge.find('.editor'), canvas);
     return challenge.find('.run').click(function() {
       return setTimeout((function() {
         return canvas.selfAssess(function() {
-          return App.currentProgress.challengeComplete('flappy_square_stage2', 'challenge4');
+          return App.currentProgress.challengeComplete('flappy_square_stage2', "challenge" + challengeIndex);
         });
       }), 2000);
     });
   };
 
   initFlappySquareStage2Challenge5 = function(page) {
-    var canvas, challenge, checkMultipleTimes, editor, points, successCount;
-    challenge = page.find('#challenge5');
+    var canvas, challenge, challengeIndex, editor;
+    challengeIndex = 5;
+    challenge = page.find("#challenge" + challengeIndex);
+    canvas = new App.Canvas(challenge.find('canvas'));
+    editor = new App.Editor(challenge.find('.editor'), canvas);
+    return challenge.find('.run').click(function() {
+      return setTimeout((function() {
+        return canvas.selfAssess(function() {
+          return App.currentProgress.challengeComplete('flappy_square_stage2', "challenge" + challengeIndex);
+        });
+      }), 2000);
+    });
+  };
+
+  initFlappySquareStage2Challenge6 = function(page) {
+    var canvas, challenge, challengeIndex, checkMultipleTimes, editor, points, successCount;
+    challengeIndex = 6;
+    challenge = page.find("#challenge" + challengeIndex);
     canvas = new App.Canvas(challenge.find('canvas'));
     editor = new App.Editor(challenge.find('.editor'), canvas);
     points = [
@@ -124,7 +145,7 @@
           var message;
           if (success) {
             message = '<strong>Success!</strong> You\'ve cleared the area outside of the boundary!';
-            App.currentProgress.challengeComplete('flappy_square_stage2', 'challenge5');
+            App.currentProgress.challengeComplete('flappy_square_stage2', "challenge" + challengeIndex);
           } else {
             message = 'Nice try, but you need to clear all areas outside of the boundary.';
           }
